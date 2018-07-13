d5e59d2c4035a841af5114ce162f005a87514624
rahulun <36655932+rahulun@users.noreply.github.com>
2018-07-13 23:00:28 +0530

    Add files via upload

---
+++ busspass/admin2.php
@@ -0,0 +1,222 @@
+<?php
+
+    session_start();
+
+    $error = "";  
+
+    if (array_key_exists("logout", $_GET)) {
+        
+        unset($_SESSION);
+        setcookie("id", "", time() - 60*60);
+        $_COOKIE["id"] = "";  
+        
+        session_destroy();
+        
+    } else if ((array_key_exists("id", $_SESSION) AND $_SESSION['id']) OR (array_key_exists("id", $_COOKIE) AND $_COOKIE['id'])) {
+        
+        header("Location: lg.php");
+        
+    }
+
+    if (array_key_exists("submit", $_POST)) {
+        
+        include("c.php");
+        
+        if (!$_POST['username']) {
+            
+            $error .= "An username address is required<br>";
+            
+        } 
+        
+        if (!$_POST['password']) {
+            
+            $error .= "A password is required<br>";
+            
+        } 
+        
+        if ($error != "") {
+            
+            $error = "<p>There were error(s) in your form:</p>".$error;
+            
+        } else {
+            
+            if ($_POST['signUp'] == '1') {
+            
+                $query = "SELECT id FROM `buss` WHERE username = '".mysqli_real_escape_string($link, $_POST['username'])."' LIMIT 1";
+
+                $result = mysqli_query($link, $query);
+
+                if (mysqli_num_rows($result) > 0) {
+
+                    $error = "That username address is taken.";
+
+                } else {
+
+                    $query = "INSERT INTO `buss` (`username`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['username'])."', '".mysqli_real_escape_string($link, $_POST['password'])."')";
+
+                    if (!mysqli_query($link, $query)) {
+
+                        $error = "<p>Could not sign you up - please try again later.</p>";
+
+                    } else {
+
+                        $query = "UPDATE `buss` SET password = '".md5(md5(mysqli_insert_id($link)).$_POST['password'])."' WHERE id = ".mysqli_insert_id($link)." LIMIT 1";
+                        
+                        $id = mysqli_insert_id($link);
+                        
+                        mysqli_query($link, $query);
+
+                        $_SESSION['id'] = $id;
+
+                        if ($_POST['stayLoggedIn'] == '1') {
+
+                            setcookie("id", $id, time() + 60*60*24*365);
+
+                        } 
+                            
+                       
+
+                    }
+
+                } 
+                
+            } else {
+                    
+                    $query = "SELECT * FROM `buss` WHERE username = '".mysqli_real_escape_string($link, $_POST['username'])."'";
+                
+                    $result = mysqli_query($link, $query);
+                
+                    $row = mysqli_fetch_array($result);
+                
+                    if (isset($row)) {
+                        
+                        $hashedPassword = md5(md5($row['id']).$_POST['password']);
+                        
+                        if ($hashedPassword == $row['password']) {
+                            
+                            $_SESSION['id'] = $row['id'];
+                            
+                            if (isset($_POST['stayLoggedIn']) AND $_POST['stayLoggedIn'] == '1') {
+
+                                setcookie("id", $row['id'], time() + 60*60*24*365);
+
+                            } 
+
+                            header("Location: lg.php");
+                                
+                        } else {
+                            
+                            $error = "That username/password combination could not be found.";
+                            
+                        }
+                        
+                    } else {
+                        
+                        $error = "That username/password combination could not be found.";
+                        
+                    }
+                    
+                }
+            
+        }
+        
+        
+    }
+
+
+?>
+
+<?php include("h.php"); ?>
+      
+      <div class="container">
+      
+    <h1>Secret Diary</h1>
+          
+          <p><strong>Store your thoughts permanently and securely.</strong></p>
+          
+          <div id="error"><?php if ($error!="") {
+    echo '<div class="alert alert-danger" role="alert">'.$error.'</div>';
+    
+} ?></div>
+
+<form method="post" id = "signUpForm">
+    
+    <p>Interested? Sign up now.</p>
+    
+    <fieldset class="form-group">
+
+        <input class="form-control" type="username" name="username" placeholder="Your username">
+        
+    </fieldset>
+    
+    <fieldset class="form-group">
+    
+        <input class="form-control" type="password" name="password" placeholder="Password">
+        
+    </fieldset>
+    
+    <div class="checkbox">
+    
+        <label>
+    
+        <input type="checkbox" name="stayLoggedIn" value=1> Stay logged in
+            
+        </label>
+        
+    </div>
+    
+    <fieldset class="form-group">
+    
+        <input type="hidden" name="signUp" value="1">
+        
+        <input class="btn btn-success" type="submit" name="submit" value="Sign Up!">
+        
+    </fieldset>
+    
+    <p><a class="toggleForms">Log in</a></p>
+
+</form>
+
+<form method="post" id = "logInForm">
+    
+    <p>Log in using your username and password.</p>
+    
+    <fieldset class="form-group">
+
+        <input class="form-control" type="username" name="username" placeholder="Your username">
+        
+    </fieldset>
+    
+    <fieldset class="form-group">
+    
+        <input class="form-control"type="password" name="password" placeholder="Password">
+        
+    </fieldset>
+    
+    <div class="checkbox">
+    
+        <label>
+    
+            <input type="checkbox" name="stayLoggedIn" value=1> Stay logged in
+            
+        </label>
+        
+    </div>
+        
+        <input type="hidden" name="signUp" value="0">
+    
+    <fieldset class="form-group">
+        
+        <input class="btn btn-success" type="submit" name="submit" value="Log In!">
+        
+    </fieldset>
+    
+    <p><a class="toggleForms">Sign up</a></p>
+
+</form>
+          
+      </div>
+
+<?php include("f.php"); ?>
+
+
